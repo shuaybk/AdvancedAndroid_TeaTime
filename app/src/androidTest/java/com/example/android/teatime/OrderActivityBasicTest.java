fb2095b5ad6213ccf348231ba89c582f7aaae208
Shuayb Khan <shuaybk@gmail.com>
2020-01-31 11:15:51 -0500

    Completed initial test

---
+++ app/src/androidTest/java/com/example/android/teatime/OrderActivityBasicTest.java
@@ -0,0 +1,29 @@
+package com.example.android.teatime;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+@RunWith(AndroidJUnit4.class)
+public class OrderActivityBasicTest {
+    @Rule
+    public ActivityTestRule<OrderActivity> mActivityTestRule
+            = new ActivityTestRule<>(OrderActivity.class);
+
+    @Test
+    public void clickIncrementButton_ChangesQuantityAndCost(){
+        onView((withId(R.id.increment_button))).perform(click());
+
+        onView(withId(R.id.quantity_text_view)).check(matches(withText("1")));
+        onView(withId(R.id.cost_text_view)).check(matches(withText("$5.00")));
+    }
+}
